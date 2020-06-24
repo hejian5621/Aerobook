64d6f18d912bc21dfe9014fc01d14e0dea94c9e9
wzwz1997 <wzmail1997@163.com>
2020-06-24 13:02:16 +0800

    111

--- a_testMethod/test_1/test9.py
+++ a_testMethod/test_1/test9.py
@@ -1,10 +1,5 @@
 
 
-r="d"
-a ={"q":"w"}
-b ={"e":r}
-d ={}
-
-c=dict( a, **b,**d )
-
-print("c:",c)
+from pywinauto.application import Application
+app = Application(backend="uia").start("notepad.exe")
+app.UntitledNotepad.type_keys("%FX")
